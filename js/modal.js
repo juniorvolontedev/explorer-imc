6f1c181d2bcd5ca2da3c4189d7d3206f4d7e9cdd
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-03 11:02:38 -0300

    fechando modal pelo teclado

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,11 @@ export const Modal = {
 };
 
 Modal.buttonClose.onclick = () => Modal.close();
+
+window.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    Modal.close();
+  }
+}
